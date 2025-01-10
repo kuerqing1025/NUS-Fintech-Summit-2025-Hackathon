7e23933ddfdf83f87320ebff3de3241d51f9c222
Brittanywu <992183573@qq.com>
2025-01-10 20:12:52 +0800

    update delay

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,11 +5,16 @@ import Report from "../Report/Report";
 const Home = () => {
   const [contractAddress, setContractAddress] = useState("");
   const [showReport, setShowReport] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const handleDecompile = () => {
-    if (contractAddress.trim()) {
-      setShowReport(true);
-    }
+    setIsDisabled(true);
+    setTimeout(() => {
+      setIsDisabled(false);
+      if (contractAddress.trim()) {
+        setShowReport(true);
+      }
+    }, 2300);
   };
 
   if (showReport) {
@@ -26,15 +31,19 @@ const Home = () => {
       <img src="src/assets/logo.png" alt="ScanGuard Logo" />
       <div className="upload">
         <div className="input-container">
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder="Enter Network Address or Bytecode"
             value={contractAddress}
             onChange={(e) => setContractAddress(e.target.value)}
           />
         </div>
-        <button className="decompile-button" onClick={handleDecompile}>
-          Decompile
+        <button
+          className="decompile-button"
+          onClick={handleDecompile}
+          disabled={isDisabled}
+        >
+          {isDisabled ? "analysing" : "Decompile"}
         </button>
         <p>
           Input your smart contract address to start the analysis process and
